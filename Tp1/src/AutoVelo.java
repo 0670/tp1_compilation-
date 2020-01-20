1ebfe5abefd7fab982677c5ba63f6cca820ef8ab
Karim OUGUEUR <Karim OUGUEUR@LAPTOP-JF5UNPG4>
2020-01-20 23:34:10 +0100

    Lexical_Ok syntaxique_en_route

---
+++ Tp1/src/AutoVelo.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+/**
+ * La classe AutoVelo met en oeuvre l'automate d'analyse syntaxique des locations de velos
+ * Realisation par interpreteur de tables
+ * 
+ * @author Girard, Grazon, Masson
+ * novembre 2019
+ */
+
+public class AutoVelo extends Automate{
+	
+	/**
+	 * Rappel: reprise apr√®s erreur demandee sur les items lexicaux VIRG, PTVIRG et BARRE
+	 */
+
+	/** table des transitions */
+	
+	private static final int[][] TRANSIT =
+			
+		{
+		/* Etat      ADULTE DEBUT ENFANT   FIN   HEURES  IDENT  NBENTIER  VIRG PTVIRG  BARRE AUTRES  */
+		/* 0 */     {  -1,   -1,    -1,     -1,    -1,     1,      -1,    -1,   -1,    12,    -1   },
+		/* 1 */     {  -1,    4,    -1,     -1,    -1,    -1,       2,    -1,   -1,    12,    -1   },
+		/* 2 */     {  -1,   -1,    -1,     -1,     3,    -1,      -1,    -1,   -1,    12,    -1   },
+		/* 3 */     {  -1,	  4,    -1,      5,    -1,    -1,      -1,    -1,   -1,    12,    -1   },
+		/* 4 */     {  -1,   -1,    -1,     -1,    -1,    -1,       6,    -1, 	-1,    12,    -1   }, // ‡ verifier		
+		/* 5 */     {  -1,   -1,    -1,   	 1,    -1,    -1, 	   -1,     0,   10,    12,    -1   },
+		/* 6 */     {   7,   -1,    -1,     -1,    -1,    -1,      -1,    -1,   -1,    12,    -1   },
+		/* 7 */     {  -1,   -1,    -1,     -1,    -1,     0,       8,    -1,   10,    12,    -1   },
+		/* 8 */     {  -1,	 -1,     9,      5,    -1,    -1,      -1,    -1,   -1,    12,    -1   },
+		/* 9 */     {  -1,   -1,    -1,     -1,    -1,    -1,      -1,     0, 	10,    12,    -1   }, 
+		/* 10 */    {  -1,   -1,    -1,   	-1,    -1,    -1, 	   -1,    -1,   -1,    12,    -1   },// ‡ verifier		
+		/* 11 */    { } // ‡ verifier		
+		//TODO completer TRANSIT
+		/* ...		{...} */    
+
+		};
+
+	/** gestion de l'affichage sur la fenetre de trace de l'execution */
+	public void newObserver(ObserverAutomate oAuto, ObserverLexique oLex ){
+		this.newObserver(oAuto);
+		this.analyseurLexical.newObserver(oLex);
+		analyseurLexical.notifyObservers(((LexVelo)analyseurLexical).getcarLu());
+	}
+	/** fin gestion de l'affichage sur la fenetre de trace de l'execution */
+
+	/**
+	 *  constructeur classe AutoVelo pour l'application Velo
+	 *  
+	 * @param flot : donnee a analyser
+	 */
+	public AutoVelo(InputStream flot) {
+		/** on utilise ici un analyseur lexical de type LexVelo */
+		analyseurLexical = new LexVelo(flot);
+		/** initialisation etats particuliers de l'automate fini d'analyse syntaxique*/
+		this.etatInitial = 0;
+		this.etatFinal = TRANSIT.length;
+		this.etatErreur = TRANSIT.length - 1;
+	}
+
+	/** definition de la methode abstraite getTransition de Automate 
+	 * 
+	 * @param etat : code de l'etat courant
+	 * @param unite : code de l'unite lexicale courante
+	 * @return code de l'etat suivant
+	 **/
+	int getTransition(int etat, int unite) {
+		return this.TRANSIT[etat][unite];
+	}
+
+	/** ici la methode abstraite faireAction de Automate n'est pas encore definie */
+	void faireAction(int etat, int unite) {};
+
+	/** ici la methode abstraite initAction de Automate n'est pas encore definie */
+	void initAction() {};
+
+	/** ici la methode abstraite getAction de Automate n'est pas encore definie */
+	int getAction(int etat, int unite) {return 0;};
+
+}
---
+++ Tp1/src/LexVelo.java
@@ -0,0 +1,249 @@
+
+/**
+ * La classe LexVelo implemente un analyseur lexical pour une application de location de velos
+ * 
+ * @author Girard, Grazon, Masson
+ * novembre 2019
+ */
+
+import java.io.InputStream;
+
+public class LexVelo extends Lex {
+
+	/** codage des items lexicaux */
+	protected final int ADULTE = 0, DEBUT = 1, ENFANT = 2, FIN = 3, HEURES = 4, IDENT = 5, NBENTIER = 6, VIRG = 7,
+			PTVIRG = 8, BARRE = 9, AUTRES = 10;
+	/** tableau de l'affichage souhaite des items lexicaux */
+	public static final String[] images = { "ADULTE", "DEBUT", "ENFANT", "FIN", "HEURES", "IDENT", "NBENT", "  ,  ",
+			"  ;  ", "  /  ", "AUTRE" };
+
+	/** nombre de mots reserves dans l'application Velo */
+	private final int NBRES = 5;
+
+	/** attributs lexicaux associes resp. a NBENTIER et IDENT */
+	private int valEnt, numIdCourant;
+
+	/** methodes d'acces aux attributs lexicaux */
+	public int getvalEnt() {
+		return this.valEnt;
+	}
+
+	public int getNumIdCourant() {
+		return this.numIdCourant;
+	}
+
+	/** caractere courant */
+	private char carLu;
+
+	/** methode d'acces a l'attribut caractere courant */
+	public char getcarLu() {
+		return this.carLu;
+	}
+
+	/**
+	 * constructeur classe LexVelo
+	 * 
+	 * @param flot : donnee d'entree a analyser
+	 */
+	public LexVelo(InputStream flot) {
+		/** initialisation du flot par la classe abstraite */
+		super(flot);
+		System.out.println("Corrige LexVelo");
+		System.out.println("--------------");
+
+		/** prelecture du premier caractere de la donnee */
+		lireCarLu();
+
+		/** initialisation de tabIdent par mots reserves de l'application Velo */
+		this.tabIdent.add(0, "ADULTE");
+		this.tabIdent.add(1, "DEBUT");
+		this.tabIdent.add(2, "ENFANT");
+		this.tabIdent.add(3, "FIN");
+		this.tabIdent.add(4, "HEURES");
+	}
+
+	/**
+	 * methode de lecture d'un nouveau caractere courant carLu a partir de la donnee
+	 * en entree flot
+	 */
+	private void lireCarLu() {
+		carLu = Lecture.lireChar(flot);
+		this.notifyObservers(carLu);
+		/**
+		 * transformation de tous les caracteres de separation en espaces et forcage
+		 * lettres en majuscules
+		 */
+		if ((carLu == '\r') || (carLu == '\n') || (carLu == '\t'))
+			carLu = ' ';
+		if (Character.isWhitespace(carLu))
+			carLu = ' ';
+		else
+			carLu = Character.toUpperCase(carLu);
+	}
+
+	/**
+	 * lecture item lexical NBENTIER et maj de l'attribut lexical valEnt
+	 * 
+	 * @return code NBENTIER
+	 */
+	private int lireEnt() {
+		String s = "";
+		do {
+			s = s + carLu;
+			lireCarLu();
+		} while ((carLu >= '0') && (carLu <= '9'));
+		valEnt = Integer.parseInt(s);
+		return NBENTIER;
+	}
+	/**
+	* lecture item lexical de l'attribut lexical numIdCourant 
+	 * 
+	 * @return le token de item correspendant au string lu 
+	 */
+	private int lireIdent() {
+		String s = "";
+		do {
+			s = s + carLu;
+			lireCarLu();
+		} while (Character.isLetter(carLu));
+		
+		int k;
+		switch (s) {
+		 	case "ADULTE":
+		 		k=0;
+				break;
+			
+		 	case "DEBUT":
+		 		k=1;
+		 		break;	
+		 	case "ENFANT":
+				k=2;
+				break;
+		 	case "FIN":
+				k=3;
+				break;
+		 	case "HEURES":
+				k=4;
+				break;
+			default: 
+				k=5;
+				
+		}
+		numIdCourant=k;
+		return  k;
+     // deuxieme methodes...
+	/*	if ( s.compareTo("ADULTE")==0) {
+			numIdCourant=0;
+			return 0;
+		}
+		if (s.compareTo("DEBUT")==0) {
+			numIdCourant=1;
+			return 1;
+		}if (s.compareTo("ENFANT")==0) {
+			numIdCourant=2;
+			return 2;
+		}if (s.compareTo("FIN")==0) {
+			numIdCourant=3;
+			return 3;
+		}if (s.compareTo("HEURES")==0) {
+			numIdCourant=4;
+			return 4;
+		}
+		if (s.compareTo("IDENT")==0) {
+			numIdCourant=5;
+			return 5;
+		
+		}
+		return numIdCourant; */
+	}
+
+	/**
+	 * definition de la methode abstraite lireSymb de Lex
+	 * 
+	 * @return code de l'item lexical detecte
+	 */
+	public int lireSymb() {
+
+		/** on "avale" espaces ou assimiles */
+		while (carLu == ' ')
+			lireCarLu();
+		/** detection item lexical NBENTIER */
+		if ((carLu >= '0') && (carLu <= '9'))
+			return lireEnt();
+		if ((carLu >= 'A') && (carLu <= 'Z'))
+			return lireIdent();
+
+		/** detection autres items lexicaux */
+		switch (carLu) {
+		case ';':
+			lireCarLu();
+			return PTVIRG;
+		case '/':
+			lireCarLu();
+			return BARRE;
+		case ',':
+			lireCarLu();
+			return VIRG;
+
+		default: {
+			System.out.println("LexVelo : incorect caracter : " + carLu);
+			lireCarLu();
+			return AUTRES;
+		}
+		}
+	}
+
+	/**
+	 * definition de la methode abstraite chaineIdent de Lex
+	 * 
+	 * @param numIdent : numero d'un ident dans la table des idents
+	 * @return chaine correspondant a numIdent
+	 */
+	public String chaineIdent(int numIdent) {
+
+		return this.tabIdent.get(numIdent);
+
+	}
+
+	/** utilitaire de test de l'analyseur lexical seul (sans analyse syntaxique) */
+	private void testeur_lexical() {
+		/** Unite lexicale courante */
+		int token;
+		/**
+		 * definition du caractere de fin de chaine utile uniquement pour test autonome
+		 * du lexical
+		 */
+		int finDeChaine = BARRE;
+		do {
+			token = lireSymb();
+			if (token == NBENTIER)
+				Lecture.attenteSurLecture("token : " + images[token] + " attribut valEnt = " + valEnt);
+			else if (token == IDENT)
+				Lecture.attenteSurLecture("token : " + images[token] + " attribut numIdCourant = " + numIdCourant);
+			else
+				Lecture.attenteSurLecture("token : " + images[token]);
+		} while (token != finDeChaine);
+	}
+
+	/**
+	 * Main pour tester l'analyseur lexical seul (sans analyse syntaxique)
+	 */
+	public static void main(String args[]) {
+
+		String nomfich;
+		nomfich = Lecture.lireString("nom du fichier d'entree : ");
+		InputStream flot = Lecture.ouvrir(nomfich);
+		if (flot == null) {
+			System.exit(0);
+		}
+
+		LexVelo testVelo = new LexVelo(flot);
+		testVelo.testeur_lexical();
+
+		Lecture.fermer(flot);
+		Lecture.attenteSurLecture("fin d'analyse");
+		System.exit(0);
+
+	}
+
+}
